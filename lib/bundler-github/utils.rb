201caeda33c8b0784f779ddd0606a69b251adbdb
Martin Schürrer <martin@schuerrer.org>
2010-09-14 14:03:09 +0200

    supress warnings

---
+++ lib/bundler-github/utils.rb
@@ -0,0 +1,10 @@
+module Kernel
+  def suppress_warnings
+    original_verbosity = $VERBOSE
+    $VERBOSE = nil
+    result = yield
+    $VERBOSE = original_verbosity
+    return result
+  end
+end
+
